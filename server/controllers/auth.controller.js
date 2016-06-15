d41afe8c961360068b65b6f712f10d5149096cf2
vai <ncysatnaf@gmail.com>
2016-06-15 17:56:43 +0800

    simplify

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -11,13 +11,8 @@ export const login = async (req,res) => {
       let isMatch = await bcrypt.compareSync(password, user.password)
       if (isMatch) {
         let token = await jwt.sign({userId:user._id}, 'ha' )
-        let update = await User.update({token: token})
-        if(update){
-          let updateUser = await User.findOne({email:email})
-          res.json(updateUser)
-        }else {
-          res.status(403).end()
-        }
+        let update = await User.findOneAndUpdate({token: token})
+        res.json(update)
     }else if (!isMatch) {
         res.status(403).send("Iniviald password")
       }
